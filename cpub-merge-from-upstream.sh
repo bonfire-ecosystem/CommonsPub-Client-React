b1b1736dd24bd57c619a29cbc0ba2a60f6b48a58
Mayel <git@mayel.space>
2020-05-21 09:46:16 +0200

    update cpub script

--- cpub-merge-from-upstream.sh
+++ cpub-merge-from-upstream.sh
@@ -1,29 +1,28 @@
-# This script helps merge upstream updates while preserving local customisations
+echo "This script helps merge upstream updates while preserving local customisations"
 
-# Check out the MoodleNet branch you wish to merge to
+echo "Check out our MoodleNet branch"
 git checkout flavour/moodlenet
 
-# Pull changes from upstream
+echo "Pull changes from upstream"
 git pull https://gitlab.com/moodlenet/frontend.git develop 
 
-# Copy upstream changes to our MoodleNet branch
+echo "Copy upstream changes to our MoodleNet branch"
 git push
 
-# Check out the CommonsPub branch you wish to merge to
+echo "Check out flavour/commonspub branch"
 git checkout flavour/commonspub
 
-# merge MoodleNet to CommonsPub, but don't commit
+echo "Merge MoodleNet to CommonsPub, without commiting yet"
 git merge --no-ff --no-commit flavour/moodlenet
 
-# Restore files which we don't want overwritten (add any core files that should be different in CommonsPub to the below line)
+echo "Restore files which we don't want overwritten (add any core files that should be different in CommonsPub to the below line in the script)"
 for file in cpub-merge-from-upstream.sh README.md src/mn-constants.tsx public/index.html .env.example .env.secrets.example docker-compose.yml docker-compose.pi.yml .gitlab-ci.yml
 do
     git reset HEAD ${file}
     git checkout -- ${file}
 done
 
-
-echo "Please check if everything looks good (including resolved any merge conflicts) before continuing. Press 'c' to merge and push the changes, or just enter to abort."
+echo "Please check if everything looks good (including resolving any merge conflicts) before continuing. Press 'c' to merge and push the changes, or just enter to abort."
 read answer
 if echo "$answer" | grep -iq "^c" ; then
     echo "Merging and pushing..."
